db5a0600803dbff8931d08858abfb89265eb950b
Theo602 <110599214+Theo602@users.noreply.github.com>
2023-04-28 18:04:43 +0200

    ajout du 1er test
    creation client http - requete url et reponse

--- tests/Controller/DiaryControllerTest.php
+++ tests/Controller/DiaryControllerTest.php
@@ -2,20 +2,34 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DiaryControllerTest extends WebTestCase
 {
 
     private KernelBrowser|null $client = null;
+    private $userRepository = null;
+    private $user = null;
+    private $urlGenerator = null;
 
     public function setUp(): void
     {
         $this->client = static::createClient();
+        $this->userRepository = $this->client->getContainer()->get('doctrine.orm.entity_manager')->getRepository(User::class);
+        $this->user = $this->userRepository->findOneByEmail('email');
+        $this->urlGenerator = $this->client->getContainer()->get('router.default');
+
+        //Authentification sur Symfony pour le test avec le user récupéré en base
+        $this->client->loginUser($this->user);
     }
 
     public function testHomepageIsUp()
     {
+        $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate('homepage'));
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
     }
 }
